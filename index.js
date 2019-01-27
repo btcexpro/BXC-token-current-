f38676928746c1668050548714759bebf32b9cfb
Hussain C <araneux@Hussains-MacBook-Pro.local>
2019-01-27 13:53:02 +0530

    Deployment script working with version

--- index.js
+++ index.js
@@ -10,10 +10,22 @@ nconf.argv().env().file({ file: `./config/config-${network}.json` });
 const BigNumber = web3.BigNumber;
 BigNumber.config({ DECIMAL_PLACES: 18, ROUNDING_MODE: BigNumber.ROUND_DOWN });
 
+const deployer = require('./utils/deployer.js');
+
 async function setup() {
-	console.log(web3.version);
-	console.log(web3.BigNumber);
-	return;
+	console.log("<< Deploy : Begin >>")
+	const owner = nconf.get("owner")
+
+	await deployer.validate(network, owner);
+	const bxcInstance = await deployer.deployBXC(network)
+	const bxcSaleInstance = await deployer.deployBXCSale(network, bxcInstance, 1549584000)
+	const bxcTeamVestingInstance = await deployer.deployBXCTeamVesting(network, bxcInstance)
+
+	await deployer.mintTokens(network, bxcInstance, owner)
+	await deployer.finishMinting(network, bxcInstance)
+		
+	await deployer.printContracts(network, bxcInstance, bxcSaleInstance, bxcTeamVestingInstance)
+	console.log("<< Deploy : Ready >>")
 }
 
 module.exports = callback => setup()
