d4c864028ab8ea4512bfb52e410d3ca1d1e7dbe3
Hussain C <araneux@Hussains-MacBook-Pro.local>
2019-01-27 22:12:06 +0530

    Implemented test case for token and sale

--- test/BXCTeamVesting.js
+++ test/BXCTeamVesting.js
@@ -1,22 +1,25 @@
-// 'use strict';
-// const deployer = require('../utils/deployer.js');
-// const { BN, constants, expectEvent, shouldFail, ether } = require('openzeppelin-test-helpers');
+'use strict';
+const deployer = require('../utils/deployer.js');
+const { BN, constants, expectEvent, shouldFail, ether } = require('openzeppelin-test-helpers');
 
-// var appInstance;
-// var bxcInstance;
-// var bxcSaleInstance;
-// var bxcTeamVestingInstance;
+var appInstance;
+var bxcInstance;
+var bxcSaleInstance;
+var bxcTeamVestingInstance;
 
-// var network, owner, wallet;
-// var initialBalances;
+var network, owner, wallet;
+var initialBalances;
 
-// var day = 60 * 60 * 24;
-// var month = day * 30;
-// var year = day * 365;
+var day = 60 * 60 * 24;
+var month = day * 30;
+var year = day * 365;
 
-// contract('BXCTeamVesting' , (accounts) => {
-// 	beforeEach(async () => {
-// 		// deploy contracts
-// 		({ network, owner, bxcInstance, bxcSaleInstance, bxcTeamVestingInstance, initialBalances } = await deployer.deploy(web3, accounts));
-// 	});
-// });
+contract('BXCTeamVesting' , (accounts) => {
+	beforeEach(async () => {
+		// saleStart
+		saleStart = new BigNumber(await TimeHelper.getBlockTime() + 60);
+
+		// deploy contracts
+		({ network, owner, bxcInstance, bxcSaleInstance, bxcTeamVestingInstance, initialBalances } = await deployer.deploy(web3, accounts, saleStart));
+	});
+});
